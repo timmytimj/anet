26387e28fe5a5f2058492f7499ef6789e511669e
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-08-20 17:06:05 +0300

    NCI-Agency/anet#50: Add tasks and assessments to print page

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -35,6 +35,7 @@ import _isEmpty from "lodash/isEmpty"
 import _upperFirst from "lodash/upperFirst"
 import { Comment, Person, Position, Report, Task } from "models"
 import moment from "moment"
+import ReportPrint from "pages/reports/Print"
 import pluralize from "pluralize"
 import PropTypes from "prop-types"
 import React, { useContext, useState } from "react"
@@ -276,7 +277,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
   const [saveSuccess, setSaveSuccess] = useState(null)
   const [saveError, setSaveError] = useState(null)
   const [showEmailModal, setShowEmailModal] = useState(false)
-
+  const [shouldPrint, setShouldPrint] = useState(false)
   const { uuid } = useParams()
   const { loading, error, data, refetch } = API.useApiQuery(GQL_GET_REPORT, {
     uuid
@@ -371,6 +372,14 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
       initialValues={report}
     >
       {({ isSubmitting, setSubmitting, isValid, setFieldValue, values }) => {
+        if (report && shouldPrint) {
+          return (
+            <ReportPrint
+              report={report}
+              setPrintDone={() => setShouldPrint(false)}
+            />
+          )
+        }
         const action = (
           <div>
             {canEmail && (
@@ -973,7 +982,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
   }
 
   function onPrintClick() {
-    history.push(`${report.uuid}/print`)
+    setShouldPrint(true)
   }
 
   function handleEmailValidation(value) {
