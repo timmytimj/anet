5b22316e2f7b15bbb46e11ca693bb3516803cb6d
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-08-28 09:12:34 +0300

    NCI-Agency/anet#50: Add test case, remove extra css file
    Rename components and class names consistently

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -35,8 +35,7 @@ import _isEmpty from "lodash/isEmpty"
 import _upperFirst from "lodash/upperFirst"
 import { Comment, Person, Position, Report, Task } from "models"
 import moment from "moment"
-// import ReportPrint from "pages/reports/Print"
-import ReportPrint from "pages/reports/Print"
+import ReportPrintView from "pages/reports/Print"
 import pluralize from "pluralize"
 import PropTypes from "prop-types"
 import React, { useContext, useState } from "react"
@@ -375,7 +374,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
       {({ isSubmitting, setSubmitting, isValid, setFieldValue, values }) => {
         if (report && shouldPrint) {
           return (
-            <ReportPrint
+            <ReportPrintView
               report={report}
               setPrintDone={() => setShouldPrint(false)}
             />
