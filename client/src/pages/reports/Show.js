607ee8b693b1ff65a378631e18318b2698678e0e
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-09-15 07:51:56 +0300

    NCI-Agency/anet#50: Add report.detailedView to anet.yml
    Fix tests failing due to renaming ui
    
    Add ANET_CONFIG env export to wdio test script

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -977,7 +977,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
 
   function onCompactClick() {
     if (!_isEmpty(report)) {
-      history.push(`${report.uuid}/compact`, { report })
+      history.push(`${report.uuid}/compact`)
     }
   }
 
