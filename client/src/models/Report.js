df1d0d1ccc1ebc0f91c0845c3a60f965a63469ae
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-10-13 10:22:13 +0300

    NCI-Agency/anet#3024: Fix test failing - text formatting, refactoring

--- client/src/models/Report.js
+++ client/src/models/Report.js
@@ -423,14 +423,6 @@ export default class Report extends Model {
     return attendees.find(el => el.role === role && el.primary)
   }
 
-  static isEngagementAllDay(report) {
-    return !report.duration
-  }
-
-  static getAllDayIndicator(report) {
-    return Report.isEngagementAllDay(report) ? "(all day)" : ""
-  }
-
   static getEngagementDateFormat() {
     return Settings.engagementsIncludeTimeAndDuration
       ? Settings.dateFormats.forms.displayLong.withTime
@@ -537,6 +529,14 @@ export default class Report extends Model {
     )
   }
 
+  static isEngagementAllDay(report) {
+    return !report.duration
+  }
+
+  static getAllDayIndicator(report) {
+    return Report.isEngagementAllDay(report) ? " (all day)" : ""
+  }
+
   static getFormattedEngagementDate(report) {
     if (!report?.engagementDate) {
       return ""
@@ -546,7 +546,7 @@ export default class Report extends Model {
     if (Report.isEngagementAllDay(report)) {
       return Settings.engagementsIncludeTimeAndDuration
         ? start.format(Settings.dateFormats.forms.displayLong.date) +
-            " (all day)"
+            Report.getAllDayIndicator(report)
         : start.format(Report.getEngagementDateFormat())
     }
 
